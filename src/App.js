5f5227a19116f5326fa5315f2d9d69506ebd1274
Jordan Zhang <jordanzhang@gmail.com>
2019-02-03 17:04:36 -0800

    Successful data fetch. More environment setup.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,49 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
+import { useState, useEffect } from "react";
+import Header from "./components/Header";
+import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+const App = () => {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const fetchData = async () => {
+    const result = await axios
+      .get(
+        `https://api.bestbuy.com/v1/products((search=LG)&(categoryPath.id=abcat0101000))?apiKey=sUu2r5kFBOd8VPsYvCuGdBbb&sort=thumbnailImage.asc&show=thumbnailImage,addToCartUrl,image,name,regularPrice,salePrice,shortDescription,type,sku&format=json`
+      )
+      .then(response => response.data.products);
+
+    console.log(result);
+
+    setProducts(result);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
   }
-}
+
+  return (
+    <div>
+      <Header />
+      <div className="flex flex-wrap">
+        {products.map(product => {
+          return (
+            <div key={product.sku} className="mw-20 ma3">
+              <img className="product-image" src={product.image} alt={product.name} />
+              <p>{product.name}</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Header = () => {
+  return <h1 className="ml2 mr2 f1">Welcome to Captain Hook's online store</h1>;
+};
+
+export default Header;
