eb8772839018b89d3e778c3b59c8ff4a3175d27e
Jordan Zhang <jordanzhang@gmail.com>
2019-02-07 00:33:15 -0800

    Style product page.

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import useFetchApi from "../hooks/useFetchApi";
-import { Heading, Pane, Paragraph } from "evergreen-ui";
+import { Heading, Pane, Button, UnorderedList, ListItem } from "evergreen-ui";
 import { PageWrapper } from "../styles/global";
+import Loading from "../components/Loading";
 
 const Product = props => {
   const {
@@ -19,27 +20,40 @@ const Product = props => {
     <PageWrapper>
       {isError && <div>Something went wrong...</div>}
       {isLoading ? (
-        <div>Loading...</div>
+        <Loading />
       ) : (
         <Pane>
-          <Heading size={600} marginTop="default">
+          <Heading size={600} marginY="default">
             {products[0].name}
           </Heading>
           <Pane>
-            <img src={products[0].image} alt={products[0].name} />
-            <Paragraph>
-              Barkadeer parrel holystone draught crack Jennys tea cup rum wench nipper rope's // end
-              take a caulk.Loot avast walk the plank Buccaneer yardarm barkadeer yawl // keelhaul
-              boatswain nipperkin.Smartly jib jack brig reef reef sails bounty // rigging splice the
-              main brace Admiral of the Black.Dead men tell no tales wench // chase guns sutler Sail
-              ho fire ship gun case shot Barbary Coast coffer. // Pressgang lanyard heave to
-              gunwalls galleon jolly boat crimp Blimey lad mizzen. // Me jib square - rigged trysail
-              lugger keelhaul boatswain Barbary Coast piracy Sail // ho.No prey, no pay jury mast
-              reef sails spyglass quarterdeck rutters Chain Shot // heave down fire ship
-              broadside.Sail ho main sheet stern chase bring a spring // upon her cable interloper
-              bounty black spot sloop driver.Yawl grog rutters Cat // o'nine tails gangway pillage
-              crimp black jack squiffy Chain Shot.
-            </Paragraph>
+            <Pane display="flex" flexWrap="wrap">
+              <Pane flex="0 1 auto" marginBottom="1.5rem">
+                <img src={products[0].image} alt={products[0].name} />
+              </Pane>
+
+              <Button
+                appearance="primary"
+                intent="none"
+                color="#ffffff"
+                marginTop="0"
+                marginX="1.5rem"
+                height={40}
+                onClick={() => {}}
+              >
+                Loot Item
+              </Button>
+            </Pane>
+
+            <UnorderedList size={500} marginY="default" minWidth="50vw" maxWidth="70vw">
+              {products[0].features.map((chunk, index) => {
+                return (
+                  <ListItem key={index} icon="caret-right">
+                    {chunk.feature}
+                  </ListItem>
+                );
+              })}
+            </UnorderedList>
           </Pane>
         </Pane>
       )}
